5631c2767ad4e6ae224681f4c510ea12e8d9ea23
Ahmdfdhilah <ahmad.121140173@student.itera.ac.id>
2025-10-08 20:53:48 +0700

    update comments

--- bubble_sort_v3.py
+++ bubble_sort_v3.py
@@ -2,7 +2,8 @@ def bubble_sort(arr):
     n = len(arr)
     for i in range(n - 1):
         flag = False # pakai flag mendeteksi apakah ada pertukaran dalam pass ini
-        for j in range(n - i - 1): # kurangi 1
+        for j in range(n - i - 1): 
+            # kurangi i karna elemen terbesar sudah pasti berada di posisi paling kanan, jadi tak perlu diperiksa
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 flag = True #true kan karna ada pertukaran
